1b89cad2ffbec563d97b24802c94ac6d2667bb93
Stéphane Noutsa <stephanenoutsak@gmail.com>
2023-11-28 22:59:00 -0800

    Testing...

--- main.tf
+++ main.tf
@@ -1,6 +1,20 @@
 # Create MongoDB Atlas Cloud Provider Access
-resource "mongodbatlas_cloud_provider_access" "access" {
-  project_id           = var.atlas_project_id
-  provider_name        = var.provider_name
-  iam_assumed_role_arn = var.iam_assumed_role_arn
+# resource "mongodbatlas_cloud_provider_access" "access" {
+#   project_id           = var.atlas_project_id
+#   provider_name        = var.provider_name
+#   iam_assumed_role_arn = var.iam_assumed_role_arn
+# }
+
+resource "mongodbatlas_cloud_provider_access_setup" "setup" {
+  project_id    = var.atlas_project_id
+  provider_name = var.provider_name
+}
+
+resource "mongodbatlas_cloud_provider_access_authorization" "auth_role" {
+  project_id = mongodbatlas_cloud_provider_access_setup.setup.project_id
+  role_id    = mongodbatlas_cloud_provider_access_setup.setup.role_id
+
+  aws {
+    iam_assumed_role_arn = var.iam_assumed_role_arn # "arn:aws:iam::772401394250:role/test-user-role"
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "role_id" {
-  value = mongodbatlas_cloud_provider_access.access.role_id
+  # value = mongodbatlas_cloud_provider_access.access.role_id
+  value = mongodbatlas_cloud_provider_access_setup.setup.role_id
+}
+
+output "atlas_aws_account_arn" {
+  value = mongodbatlas_cloud_provider_access_setup.setup.aws_config.atlas_aws_account_arn
 }
